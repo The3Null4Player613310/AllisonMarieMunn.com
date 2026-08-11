c98183e192ce15d0c3ddbc7ac30c50ba3dead504
Allison Munn <the.3null.4player.6.13.3.10.@gmail.com>
2026-08-11 20:25:34 -0300

    Update form.js
    Updated: form.js

--- js/form.js
+++ js/form.js
@@ -37,7 +37,7 @@ function submit(event)
   document.cookie = "LNAME='"+data["LNAME"]+"'; path=/; max-age=8; samesite=strict";
   document.cookie = "PHONE='"+data["PHONE"]+"'; path=/; max-age=8; samesite=strict";
   document.cookie = "EMAIL='"+data["EMAIL"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie = "SNAME='"+data["SNAME"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "RNAME='"+data["RNAME"]+"'; path=/; max-age=8; samesite=strict";
   document.cookie = "CNAME='"+data["CNAME"]+"'; path=/; max-age=8; samesite=strict";
   document.cookie = "PNAME='"+data["PNAME"]+"'; path=/; max-age=8; samesite=strict";
   document.cookie = "PCODE='"+data["PCODE"]+"'; path=/; max-age=8; samesite=strict";
