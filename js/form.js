70ac8601c40fa08c1d930651f63b6639423d2480
Allison Munn <the.3null.4player.6.13.3.10.@gmail.com>
2026-08-11 16:12:25 +0000

    Update form.js
    Updated: form.js

--- js/form.js
+++ js/form.js
@@ -32,26 +32,26 @@ function submit(event)
     }
   }
 
-  document.cookie += "FNAME='"+data["FNAME"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "MNAME='"+data["MNAME"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "LNAME='"+data["LNAME"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "PHONE='"+data["PHONE"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "EMAIL='"+data["EMAIL"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "SNAME='"+data["SNAME"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "CNAME='"+data["CNAME"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "PNAME='"+data["PNAME"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "PCODE='"+data["PCODE"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "EFNAME='"+data["EFNAME"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "EPHONE='"+data["EPHONE"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "EEMAIL='"+data["EEMAIL"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "DNAME='"+data["DNAME"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "SNAME='"+data["SNAME"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "HOURS='"+data["HOURS"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "DATE='"+data["DATE"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "WNAME='"+data["WNAME"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "METHOD='"+data["METHOD"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "SUUID='"+data["SUUID"]+"'; path=/; max-age=8; samesite=strict";
-  document.cookie += "ETIME='"+data["ETIME"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "FNAME='"+data["FNAME"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "MNAME='"+data["MNAME"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "LNAME='"+data["LNAME"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "PHONE='"+data["PHONE"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "EMAIL='"+data["EMAIL"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "SNAME='"+data["SNAME"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "CNAME='"+data["CNAME"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "PNAME='"+data["PNAME"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "PCODE='"+data["PCODE"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "EFNAME='"+data["EFNAME"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "EPHONE='"+data["EPHONE"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "EEMAIL='"+data["EEMAIL"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "DNAME='"+data["DNAME"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "SNAME='"+data["SNAME"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "HOURS='"+data["HOURS"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "DATE='"+data["DATE"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "WNAME='"+data["WNAME"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "METHOD='"+data["METHOD"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "SUUID='"+data["SUUID"]+"'; path=/; max-age=8; samesite=strict";
+  document.cookie = "ETIME='"+data["ETIME"]+"'; path=/; max-age=8; samesite=strict";
 
   window.location.replace("submit.html");
   event.preventDefault();
