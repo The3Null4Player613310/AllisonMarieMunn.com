044320962692253d7a82b59004485381af0c9c99
Allison Munn <the.3null.4player.6.13.3.10.@gmail.com>
2026-08-12 17:32:55 -0300

    Update submit.js
    Updated: submit.js

--- js/submit.js
+++ js/submit.js
@@ -1,10 +1,10 @@
 /*
 ################################################################
-# ALLISON MARIE MUNN: form.js                             #
-# COPYRIGHT © ALLISON MARIE MUNN                              #
-# LICENSE: GPLV2 ONLY                                        #
+# ALLISON MARIE MUNN: form.js                                  #
+# COPYRIGHT © ALLISON MARIE MUNN                               #
+# LICENSE: GPLV2 ONLY                                          #
 # FULL COPYRIGHT NOTICE IS IN README                           #
-# AUTHOR: ALLISON MARIE MUNN                                 #
+# AUTHOR: ALLISON MARIE MUNN                                   #
 ################################################################
 */
 
