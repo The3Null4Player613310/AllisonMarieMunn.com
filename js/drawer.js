bfa70fe18b358a99a3a553838cb95f266da9168d
Allison Munn <the.3null.4player.6.13.3.10.@gmail.com>
2026-08-12 11:25:16 -0300

    Create drawer.js
    Created: drawer.js

---
+++ js/drawer.js
@@ -0,0 +1,44 @@
+/*
+################################################################
+# ALLISON MARIE MUNN: drawer.js                             #
+# COPYRIGHT © ALLISON MARIE MUNN                              #
+# LICENSE: GPLV2 ONLY                                        #
+# FULL COPYRIGHT NOTICE IS IN README                           #
+# AUTHOR: ALLISON MARIE MUNN                                 #
+################################################################
+*/
+
+var isOpen = false;
+var dTime = 400;
+
+function navDrawerOpen()
+{
+  if(isOpen === false)
+  {
+    $("#nav-drawer").animate({ "left":"0px" }, dTime, function() {
+      $("#nav-drawer").attr("class", "nav-drawer-open");
+    });
+    $("#nav-drawer-shim").attr("class", "nav-drawer-shim-open");
+    $("html, body").css({ "overflow": "hidden", "height": "100%"});
+    isOpen = true;
+  }
+}
+
+function navDrawerClose()
+{
+  if(isOpen === true)
+  {
+    $("#nav-drawer").animate({ "left":"-332px" }, dTime, function() {
+      $("#nav-drawer").attr("class", "nav-drawer-close");
+    });
+    $("#nav-drawer-shim").attr("class", "nav-drawer-shim-close");
+    $("html, body").css({ "overflow":"auto", "height":"auto"});
+    isOpen = false;
+  }
+}
+
+function drawer()
+{
+  $("#header-menu").click(navDrawerOpen); // change to nav-drawer-button
+  $("#nav-drawer-shim").click(navDrawerClose);
+}
